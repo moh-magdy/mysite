231b7a77d766b4dadeeebf0723b3407e373e7323
unknown <moh.mgd@gmail.com>
2018-03-26 19:06:42 +0200

    new idea

--- js/main.js
+++ js/main.js
@@ -6,13 +6,31 @@ $(document).ready(function(){
         $('.logo').css({
             'transform' : 'translate(0px, '+ wScroll /2 +'%)'
         });
+        $('.back').css({
+            'transform' : 'translate(0px, '+ wScroll /20 +'%)'
+        });
+
+        if ($(document).scrollTop() > 20) {
+
+            $('.navbar-default').css({
+               'background': '#fff',
+               'border'  : '#e7e7e7',
+               
+            });
+            $('.navbar-nav>li>a').css({
+                'color' : '#000'
+            });
+        }else {
+            $('.navbar-default').css({
+                'background': 'rgba(255, 255, 255, 0)' ,
+                'border'  : 'none'
+             });
+             $('.navbar-nav>li>a').css({
+                'color' : '#fff'
+            })
+
+        }
         
-        $('.back-bird').css({
-            'transform' : 'translate(0px, '+ wScroll /4 +'%)'
-        });
-        $('.fore-bird').css({
-            'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
-        });
 
     });
 
